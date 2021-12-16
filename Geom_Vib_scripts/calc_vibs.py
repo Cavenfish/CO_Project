040850c9540ec291728389ba24648ed5743e0940
Brian Ferrari <brianf1996@knights.ucf.edu>
2021-12-16 12:50:39 -0500

    remove __name__ guard

--- Geom_Vib_scripts/calc_vibs.py
+++ Geom_Vib_scripts/calc_vibs.py
@@ -1,82 +1,78 @@
-#!/usr/bin/env python3
+import time
+from ase.io.trajectory import Trajectory
+from ase.io import read
+system = read('optimized_pentamer.xyz')
 
-if __name__ == "__main__" :
+from ase.vibrations import Vibrations
 
-	import time
-	from ase.io.trajectory import Trajectory
-	from ase.io import read
-	system = read('optimized_pentamer.xyz')
-
-	from ase.vibrations import Vibrations
-
-	from MvH_CO_JM8 import MvH_CO
-#	from coco_MvH_ASE import coco_MvH
-#	from coco_MvH_ASE import coco_qfix_MvH
-#	from coco_MvH_ASE import coco2_MvH
-	calc = MvH_CO(atoms=system)
-	system.set_calculator(calc)
+from MvH_CO_JM8 import MvH_CO
+# from coco_MvH_ASE import coco_MvH
+# from coco_MvH_ASE import coco_qfix_MvH
+# from coco_MvH_ASE import coco2_MvH
+calc = MvH_CO(atoms=system)
+system.set_calculator(calc)
 
 
-	
-	print(system.get_potential_energy())
-	if isinstance(calc, MvH_CO) or isinstance(calc, coco2_MvH):
-		E_intra, E_pair, E_ex, E_disp, E_elst = calc.get_energy_contributions()
-		print(sum([E_intra,E_pair]), E_intra, E_pair)
-		print(sum([E_ex, E_disp, E_elst]), E_ex, E_disp, E_elst)
 
-	start = time.time()
-	analytical_forces = system.get_forces()
-	end = time.time()
-	print("Analytical forces: ", end-start, "seconds \n", analytical_forces)
+print(system.get_potential_energy())
+if isinstance(calc, MvH_CO) or isinstance(calc, coco2_MvH):
+	E_intra, E_pair, E_ex, E_disp, E_elst = calc.get_energy_contributions()
+	print(sum([E_intra,E_pair]), E_intra, E_pair)
+	print(sum([E_ex, E_disp, E_elst]), E_ex, E_disp, E_elst)
 
-	start = time.time()
-	numerical_forces = calc.calculate_numerical_forces(system, d=1E-6)
-	end = time.time()
-	print("Numerical forces: ", end-start, "seconds \n", numerical_forces)
-	
-	difference = numerical_forces - analytical_forces
-	print("Difference:")
-	print(difference)
-	import numpy as np
-	verschil = np.linalg.norm(numerical_forces - analytical_forces)
-	print(verschil)
+start = time.time()
+analytical_forces = system.get_forces()
+end = time.time()
+print("Analytical forces: ", end-start, "seconds \n", analytical_forces)
 
-	
-	vib = Vibrations(system, delta=0.0001)
-	vib.run()
-	vib.summary()
+start = time.time()
+numerical_forces = calc.calculate_numerical_forces(system, d=1E-6)
+end = time.time()
+print("Numerical forces: ", end-start, "seconds \n", numerical_forces)
+
+difference = numerical_forces - analytical_forces
+print("Difference:")
+print(difference)
+import numpy as np
+verschil = np.linalg.norm(numerical_forces - analytical_forces)
+print(verschil)
 
 
-#	from ase.optimize import BFGS
-#	dyn = BFGS(system, trajectory='Pentamer_FQ_True_JM7_fmax_e-4.opt.traj')
-#	dyn.run(fmax=0.0001)
-#	print(system.get_potential_energy())
+vib = Vibrations(system, delta=0.0001)
+vib.run()
+vib.summary()
 
-#	with open("Pentamer_FQ_True_JM7_fmax_e-4.txt", "a") as f:
-	
-#		print(system.get_potential_energy(), file=f)
-#		if isinstance(calc, MvH_CO) or isinstance(calc, coco2_MvH):
-#			E_intra, E_pair, E_ex, E_disp, E_elst = calc.get_energy_contributions()
-#			print(sum([E_intra,E_pair]), E_intra, E_pair, file=f)
-#			print(sum([E_ex, E_disp, E_elst]), E_ex, E_disp, E_elst, file=f)
 
-#		start = time.time()
-#		analytical_forces = system.get_forces()
-#		end = time.time()
-#		print("Analytical forces: ", end-start, "seconds \n", analytical_forces, file=f)
-
-#		start = time.time()
-#		numerical_forces = calc.calculate_numerical_forces(system, d=1E-6)
-#		end = time.time()
-#		print("Numerical forces: ", end-start, "seconds \n", numerical_forces, file=f)
-	
-#		difference = numerical_forces - analytical_forces
-#		print("Difference:", file=f)
-#		print(difference, file=f)
-#		import numpy as np
-#		verschil = np.linalg.norm(numerical_forces - analytical_forces)
-#		print(verschil, file=f)
-	# cross-check energy of local minimum with a differen calculator
-# 	calc = coco2_MvH(atoms=system)
-# 	system.set_calculator(calc)
-# 	print(system.get_potential_energy())
+# from ase.optimize import BFGS
+# dyn = BFGS(system, trajectory='Pentamer_FQ_True_JM7_fmax_e-4.opt.traj')
+# dyn.run(fmax=0.0001)
+# print(system.get_potential_energy())
+#
+# with open("Pentamer_FQ_True_JM7_fmax_e-4.txt", "a") as f:
+#
+# 	print(system.get_potential_energy(), file=f)
+# 	if isinstance(calc, MvH_CO) or isinstance(calc, coco2_MvH):
+# 		E_intra, E_pair, E_ex, E_disp, E_elst = calc.get_energy_contributions()
+# 		print(sum([E_intra,E_pair]), E_intra, E_pair, file=f)
+# 		print(sum([E_ex, E_disp, E_elst]), E_ex, E_disp, E_elst, file=f)
+#
+# 	start = time.time()
+# 	analytical_forces = system.get_forces()
+# 	end = time.time()
+# 	print("Analytical forces: ", end-start, "seconds \n", analytical_forces, file=f)
+#
+# 	start = time.time()
+# 	numerical_forces = calc.calculate_numerical_forces(system, d=1E-6)
+# 	end = time.time()
+# 	print("Numerical forces: ", end-start, "seconds \n", numerical_forces, file=f)
+#
+# 	difference = numerical_forces - analytical_forces
+# 	print("Difference:", file=f)
+# 	print(difference, file=f)
+# 	import numpy as np
+# 	verschil = np.linalg.norm(numerical_forces - analytical_forces)
+# 	print(verschil, file=f)
+# cross-check energy of local minimum with a differen calculator
+# calc = coco2_MvH(atoms=system)
+# system.set_calculator(calc)
+# print(system.get_potential_energy())
