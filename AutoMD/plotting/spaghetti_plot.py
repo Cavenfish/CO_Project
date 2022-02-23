97007e2e8c8d85282eaf899c6f16a2cd41952b19
Brian Carmona Ferrari <ferraribrian1996@gmail.com>
2022-02-23 11:21:44 +0100

    made code pretty

--- AutoMD/plotting/spaghetti_plot.py
+++ AutoMD/plotting/spaghetti_plot.py
@@ -1,42 +1,27 @@
 from ..config import *
 
 def spaghetti_plot(csvDir, n=51):
+    def loop(property, ext):
+        N     = len(os.listdir(csvDir))
+        y_avg = 0
+        for fname in os.listdir(csvDir):
+            if '.csv' not in fname:
+                N -= 1
+                continue
+            df = pd.read_csv(csvDir + fname)
+            x  = df['Time']/10
+            y  = savgol_filter(df[property], n, 2)
+            plt.plot(x, y)
 
-    N     = len(os.listdir(csvDir))
-    y_avg = 0
-    for fname in os.listdir(csvDir):
-        if '.csv' not in fname:
-            N -= 1
-            continue
-        df = pd.read_csv(csvDir + fname)
-        x  = df['Time']/10
-        y  = savgol_filter(df['Total Energy'], n, 2)
-        plt.plot(x, y)
+            y_avg += y
 
-        y_avg += y
+        y_avg /= N
+        plt.plot(x, y_avg, '--', label='Average')
+        plt.legend()
+        plt.savefig(csvDir + ext)
+        plt.close()
+        return
 
-    y_avg /= N
-    plt.plot(x, y_avg, '--', label='Average')
-    plt.legend()
-    plt.savefig(csvDir + 'spaghetti_plot.png')
-    plt.close()
-
-    N     = len(os.listdir(csvDir))
-    y_avg = 0
-    for fname in os.listdir(csvDir):
-        if '.csv' not in fname:
-            N -= 1
-            continue
-        df = pd.read_csv(csvDir + fname)
-        x  = df['Time']/10
-        y  = savgol_filter(df['Sliced Energy'], n, 2)
-        plt.plot(x, y)
-
-        y_avg += y
-
-    y_avg /= N
-    plt.plot(x, y_avg, '--', label='Average')
-    plt.legend()
-    plt.savefig(csvDir + 'spaghetti_plot_sliced.png')
-    plt.close()
+    loop( 'Total Energy', 'spaghetti_plot.png')
+    loop('Sliced Energy', 'spaghetti_plot_sliced.png')
     return
