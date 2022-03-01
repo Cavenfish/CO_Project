c057fbfbf81601b4364aa0c192bee11c7a6cb3f3
brian <ferraribrian1996@gmail.com>
2022-03-01 13:37:05 +0100

    started changes for new function

--- AutoMD/config.py
+++ AutoMD/config.py
@@ -460,6 +460,16 @@ def track_E_vs_r(EList, pos, masses):
 def radial_distribution(xyz):
     atoms = read(xyz)
 
-    #for i in range(len(atoms)//2):
+    pos    = atoms.get_positions()
+    masses = atoms.get_masses()
+    eCoM   = CoM(pos[:2], masses[:2])
+    rList  = []
+    for i in range(1, len(masses)//2):
+        a     = i*2
+        b     = a+2
+        cm    = CoM(pos[a:b], masses[a:b])
+        diff  = cm - eCoM
+        r     = np.sqrt(np.dot(diff,diff))
+        rList.append(r)
 
-    return
+    return rList
--- AutoMD/plotting/__init__.py
+++ AutoMD/plotting/__init__.py
@@ -6,3 +6,4 @@ from .spaghetti_plot       import spaghetti_plot
 from .energy_contributions import energy_contributions
 from .half_life            import half_life
 from .energy_spaghetti     import energy_spaghetti
+from .radial_energy        import radial_energy
