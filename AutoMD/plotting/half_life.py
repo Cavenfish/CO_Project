453513fbdeb4b90cee2881c112272b3e1cb471bb
brian <ferraribrian1996@gmail.com>
2022-03-10 18:10:39 +0100

    added new fitting function to half life function

--- AutoMD/plotting/half_life.py
+++ AutoMD/plotting/half_life.py
@@ -24,21 +24,24 @@ def half_life(csvDir, n=51):
 
     def plot(avg, ext, property):
         #Half Life formula
-        def f(x, E, tau):
-            return E * np.exp(-x / tau)
+        def f(x, E, tau, b):
+            return (E * np.exp(-x / tau))+b
         def g(x, E, r):
             return E - (r*x)
+        def h(x, E0, tau, E1, m):
+            return (E0*np.exp(-x / tau)) + E1 - m*x
 
         saveName = csvDir + ext
         x        = avg['Time']/10
         y        = avg[property]
         y2       = savgol_filter(y, n, 2)
         
-        p0 = [np.average(y2[0:100]), 1]
+        p0 = [np.average(y2[0:10]), 1, np.average(y2[-10:-1]), 1]
 
         #Run curve fit
-        popt, pcov = curve_fit(f, x, y2, p0)
-        popt2,pcov2 = curve_fit(g, x, y2, p0)
+        popt, pcov = curve_fit(f, x, y2, p0[0:3])
+        popt2,pcov2 = curve_fit(g, x, y2, p0[0:2])
+        popt3,pcov3 = curve_fit(h, x, y, p0)
 
         #Make plot text
         t1 = popt[1] * np.log(2)
@@ -50,6 +53,7 @@ def half_life(csvDir, n=51):
         plt.plot(x, y2, label='Savitzky-Golay Filter')
         plt.plot(x, f(x, *popt), label='Exponential Fit')
         plt.plot(x, g(x, *popt2), label='Linear Fit')
+        plt.plot(x, h(x, *popt3), label='Combination Fit')
         plt.text(min(x), min(y), s)
         plt.xlabel('Time (ps)')
         plt.ylabel('Energy (eV)')
@@ -58,8 +62,6 @@ def half_life(csvDir, n=51):
         plt.tight_layout()
         plt.savefig(saveName)
         plt.close()
-        return
-
         return
 
     avg = get_avg()
