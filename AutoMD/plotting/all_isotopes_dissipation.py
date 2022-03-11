bc723a91598632a95b8d5a2a6b3b0ead0a2682b0
brian <ferraribrian1996@gmail.com>
2022-03-11 14:15:47 +0100

    new function being added

---
+++ AutoMD/plotting/all_isotopes_dissipation.py
@@ -0,0 +1,50 @@
+from ..config import *
+
+def all_isotopes_dissipation(root):
+    def get_avg(csvDir):
+        keys = ['Time', 'Total Energy', 'Sliced Energy']
+        tmp  = {}
+        N    = 0
+        for fname in os.listdir(csvDir):
+            if '.csv' not in fname:
+                continue
+            N += 1
+            df = pd.read_csv(csvDir + fname)
+
+            for key in keys:
+                if key not in tmp:
+                    tmp[key]  = df[key]
+                else:
+                    tmp[key] += df[key]
+        for key in tmp:
+            tmp[key] /= N
+
+        avg = pd.DataFrame(tmp)
+        return avg
+
+    def plot(ext, property):
+        saveName = root + ext
+        labels   = {'co':'CO', '13co':r'$^{13}$CO', 
+                    '13c18o':r'$^{13}$C$^{18}$O', 'c18o':r'C$^{18}$O'}
+        for dir in os.listdir(root):
+            if not os.path.isdir(root + dir):
+                continue
+
+            csvDir  = root + dir + '/' 
+            avg     = get_avg(csvDir)
+            x       = avg['Time']
+            y       = avg[property]
+            plt.plot(x,  y, label=labels[dir])
+        
+        plt.xlabel('Time (ps)')
+        plt.ylabel('Energy (eV)')
+        plt.title('Vibrational Energy Dissipation')
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(saveName)
+        plt.close()
+        return
+    
+    plot('all_dissipation.png', 'Total Energy')
+    plot('all_dissipation_sliced.png', 'Sliced Energy')
+    return
