eb5203eb1f6a5a7e0a6aa0159ae5fe9f80b83669
brian <ferraribrian1996@gmail.com>
2022-03-15 16:50:24 +0100

    new function first commit

---
+++ AutoMD/plotting/surf_vs_subsurf.py
@@ -0,0 +1,47 @@
+from ..config import *
+
+def surf_vs_subsurf(root):
+    def get_avg(csvDir):
+        keys = ['Time', 'Total Energy', 'Sliced Energy']
+        tmp  = {}
+        N    = 0
+        for fname in os.listdir(csvDir):
+            if '.csv' not in fname:
+                continue
+            N += 1
+            df = pd.read_csv(csvDir + fname)
+
+            for key in keys:
+                if key not in tmp:
+                    tmp[key]  = df[key]
+                else:
+                    tmp[key] += df[key]
+        for key in tmp:
+            tmp[key] /= N
+
+        avg = pd.DataFrame(tmp)
+        return avg
+
+    def plot(ext, property):
+        saveName = root + ext
+        labels   = {'surf':'Surface', 'subsurf':'Sub-Surface'}
+        for dir in os.listdir(root):
+            if not os.path.isdir(root + dir):
+                continue
+
+            csvDir  = root + dir + '/'
+            avg     = get_avg(csvDir)
+            x       = avg['Time']
+            y       = avg[property]
+            plt.plot(x,  y, label=labels[dir])
+
+        plt.xlabel('Time (ps)')
+        plt.ylabel('Energy (eV)')
+        plt.title('Vibrational Energy Dissipation')
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(saveName)
+        plt.close()
+        return
+
+    return
