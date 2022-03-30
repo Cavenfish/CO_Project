3453023a1c6379285a747db2c830322d8dc99017
Brian Carmona Ferrari <ferraribrian1996@gmail.com>
2022-03-30 10:31:26 +0200

    small changes

--- AutoMD/plotting/half_life.py
+++ AutoMD/plotting/half_life.py
@@ -24,8 +24,8 @@ def half_life(csvDir, n=51):
 
     def plot(avg, ext, property):
         #Half Life formula
-        def f(x, E, tau, b):
-            return (E * np.exp(-x / tau))+b
+        def f(x, E, tau):
+            return (E * np.exp(-x / tau))
         def g(x, E, r):
             return E - (r*x)
         def h(x, E0, tau, E1, m):
@@ -37,25 +37,25 @@ def half_life(csvDir, n=51):
         x        = avg['Time']
         y        = avg[property]
         y2       = savgol_filter(y, n, 2)
-        
+
         p0 = [np.average(y2[0:10]), 1, np.average(y2[-10:-1]), 1]
 
         #Run curve fit
-        popt, pcov = curve_fit(f, x, y2, p0[0:3])
+        popt, pcov = curve_fit(f, x, y2, p0[0:2])
         popt2,pcov2 = curve_fit(g, x, y2, p0[0:2])
         popt3,pcov3 = curve_fit(h, x, y, p0)
 
         #Make plot text
         t1 = popt[1] * np.log(2)
-        t2 = (popt2[0]/2) / popt2[1]  
-        s  = r'   Exp: $t_{1/2}$ = ' + str(t1)[:5] + ' ps\n' + \
-             r'Linear: $t_{1/2}$ = ' + str(t2)[:5] + ' ps'
+        t2 = (popt2[0]/2) / popt2[1]
+        s  = r'   Exp: $t_{1/2}$ = ' + str(t1)[:5] + ' ps\n'# + \
+             #r'Linear: $t_{1/2}$ = ' + str(t2)[:5] + ' ps'
 
         plt.plot(x,  y, label='Raw Data')
-        plt.plot(x, y2, label='Savitzky-Golay Filter')
+        #plt.plot(x, y2, label='Savitzky-Golay Filter')
         plt.plot(x, f(x, *popt), label='Exponential Fit')
-        plt.plot(x, g(x, *popt2), label='Linear Fit')
-        plt.plot(x, h(x, *popt3), label='Combination Fit')
+        #plt.plot(x, g(x, *popt2), label='Linear Fit')
+        #plt.plot(x, h(x, *popt3), label='Combination Fit')
         plt.text(min(x), min(y), s)
         plt.xlabel('Time (ps)',   fontsize=15)
         plt.ylabel('Energy (eV)', fontsize=15)
