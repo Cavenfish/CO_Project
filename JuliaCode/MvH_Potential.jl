9ea3c08e7a03d053e6561f89bd972be7481a8c04
Brian Carmona Ferrari <ferraribrian1996@gmail.com>
2022-05-02 16:21:31 +0200

    Morse potential runs, need to test later if correct

--- JuliaCode/MvH_Potential.jl
+++ JuliaCode/MvH_Potential.jl
@@ -1,14 +1,51 @@
+module MvH_Potential
+
 using NBodySimulator
+using StaticArrays
 import NBodySimulator.get_accelerating_function
 
+export MorseParameters
+
 struct MorseParameters <: PotentialParameters
-  De = 11.23
-  ρ0 = 2.3281*1.1282
-  r0 = 1.1282
+  ϵ::Float64
+  ρ0::Float64
+  r0::Float64
+  # ϵ  = 11.23
+  # ρ0 = 2.3281*1.1282
+  # r0 = 1.1282
 end
 
 function get_accelerating_function(p::MorseParameters, sim::NBodySimulation)
+  #Note, dv = acceleration
+  #      u  = positions
+  #      v  = velocity
+  #      t  = time
+  #      i  = particle number
+
+  bodies = sim.system.bodies
+  m = get_masses(sim.system)
+  N      = length(bodies)
+
   (dv, u, v, t, i) -> begin
-    
+
+
+    if ( i % 2 == 0)
+      rj   = @SVector [u[1, i-1], u[2, i-1], u[3, i-1]]
+    else
+      rj   = @SVector [u[1, i+1], u[2, i+1], u[3, i+1]]
+    end
+
+    ri   = @SVector [u[1, i], u[2, i], u[3, i]]
+    diff = rj .- ri
+
+    F    = @SVector [0.0, 0.0, 0.0]
+    preF = 2 * p.ϵ * p.ρ0 / p.r0
+    r    = √(diff'diff)
+    expf = exp(p.ρ0 * (1.0 - r / p.r0))
+    F   += preF * expf * (expf - 1) * diff / r
+
+    dv  .= F / m[i]
   end #Morse acceleration
 end
+
+end #module
--- JuliaCode/tmp.jl
+++ JuliaCode/tmp.jl
@@ -1,6 +1,8 @@
 include("./AutoMD.jl")
+include("MvH_Potential.jl")
 
 using .AutoMD
+using .MvH_Potential
 using NBodySimulator
 using StaticArrays
 
@@ -8,7 +10,11 @@ l    = 10.0
 bdys = read_ase_xyz("10co.xyz")
 #pbc  = PeriodicBoundaryConditions(l)
 pbc  = InfiniteBox()
-sys  = ChargedParticles(bdys, 1.0)
+#sys  = ChargedParticles(bdys, 1.0)
+
+p = MorseParameters(11.23, 2.3281*1.1282, 1.1282)
+sys = PotentialNBodySystem(bdys, Dict(:custom_potential_params => p))
+
 sim  = NBodySimulation(sys, (0.0, 1.0), pbc)
 sr   = run_simulation(sim, VelocityVerlet(), dt=0.001)
 #NBodySimulator.save_to_pdb(sr, "test.pdb")
