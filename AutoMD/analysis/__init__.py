1b2d3a21bd2d4f7759facea127c02168e16d6d17
Brian Carmona Ferrari <ferraribrian1996@gmail.com>
2022-06-27 23:23:15 +0200

    added a new function

--- AutoMD/analysis/__init__.py
+++ AutoMD/analysis/__init__.py
@@ -5,5 +5,6 @@ AutoMD package for doing data analysis
 from .half_life        import half_life
 from .diffusion_length import diffusion_length
 from .local_geo        import local_geo
+from .unpickle         import unpickle
 
 __functions__ = [half_life, diffusion_length]
---
+++ AutoMD/analysis/unpickle.py
@@ -0,0 +1,32 @@
+from ..config import *
+
+def unpickle(file, low, hi):
+    def get_rList(atoms):
+        pos    = atoms.get_positions()
+        masses = atoms.get_masses()
+        eCoM   = CoM(pos[:2], masses[:2])
+        rList  = []
+        for i in range(0, len(masses)//2):
+            a     = i*2
+            b     = a+2
+            cm    = CoM(pos[a:b], masses[a:b])
+            diff  = cm - eCoM
+            r     = np.sqrt(np.dot(diff,diff))
+            rList.append(r)
+            rList.append(r)
+
+        return rList
+
+    traj  = Trajectory(file)
+    atoms = traj[0]
+    rList = np.array(get_rList(atoms))
+
+    tmp   = atoms[(low <= rList) & (rList <= hi)]
+    save  = file.replace('.traj', '.xyz')
+    write(save, tmp)
+
+    for frame in traj[1:]:
+        tmp = frame[(low <= rList) & (rList <= hi)]
+        write(save, tmp, append=True)
+
+    return
