a53298b0a4bbab1d941e1a21f3bb3add8f0187f0
Brian Carmona Ferrari <ferraribrian1996@gmail.com>
2022-06-28 15:52:59 +0200

    more dynamic nu function

--- AutoMD/plotting/all_nu_dissipation.py
+++ AutoMD/plotting/all_nu_dissipation.py
@@ -51,25 +51,38 @@ def all_nu_dissipation(root, noBkg=False):
                 'nu1': r'$\nu$ = 1', 'nu2': r'$\nu$ = 2', 'nu3': r'$\nu$ = 3',
                 'nu4': r'$\nu$ = 4', 'nu5': r'$\nu$ = 5', 'nu6': r'$\nu$ = 6'}
         colors   = {'surf':(1,0,1,1), 'subsurf':(0.27,0.04,0.16,1)}
+        isotope  = {'co':'CO', '13co':r'$^{13}$CO',
+                    '13c18o':r'$^{13}$C$^{18}$O', 'c18o':r'C$^{18}$O'}
         for dir in os.listdir(root):
             if not os.path.isdir(root + dir):
                 continue
 
             if dir in colors:
                 labels  = {'surf': 'Surface', 'subsurf': 'Subsurface'}
-                subRoot = root + dir + '/co/'
-                for dir2 in os.listdir(subRoot):
-                    csvDir  = subRoot + dir2 + '/'
-                    if not os.path.isdir(csvDir):
-                        continue
-                    avg     = get_avg(csvDir)
-                    x       = avg['Time']
-                    y       = avg[property]
-                    l       = labels[dir]
-                    c       = colors[dir]
-                    _, labs = plt.gca().get_legend_handles_labels()
-                    plt.plot(x,  y, color=c,
-                             label=l if l not in labs else "")
+                for iso in isotope.keys():
+                    saveName = root + iso + '_' + ext
+                    subRoot = root + dir + '/' + iso + '/'
+                    for dir2 in os.listdir(subRoot):
+                        csvDir  = subRoot + dir2 + '/'
+                        if not os.path.isdir(csvDir):
+                            continue
+                        avg     = get_avg(csvDir)
+                        x       = avg['Time']
+                        y       = avg[property]
+                        l       = labels[dir]
+                        c       = colors[dir]
+                        _, labs = plt.gca().get_legend_handles_labels()
+                        plt.plot(x,  y, color=c,
+                                 label=l if l not in labs else "")
+                plt.xlabel('Time (ps)',   fontsize=18)
+                plt.ylabel('Energy (eV)', fontsize=18)
+                plt.title('Vibrational Energy Dissipation', fontsize=20)
+                plt.legend(fontsize=12)
+                #plt.gca().get_legend().remove()
+                plt.tight_layout()
+                plt.savefig(saveName, transparent=noBkg)
+                plt.close()
+                return
             else:
                 csvDir  = root + dir + '/'
                 avg     = get_avg(csvDir)
