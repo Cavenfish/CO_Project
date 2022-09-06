2cb92f67eb0b9fa26dc48f4b2f2760162c36c0ad
brian <ferraribrian1996@gmail.com>
2022-09-06 09:39:39 +0200

    zpe push

--- AutoMD/experiments/__init__.py
+++ AutoMD/experiments/__init__.py
@@ -3,4 +3,5 @@ AutoMD package for doing experiments
 """
 from .binding_energy           import binding_energy
 from .crystal_binding_energy   import crystal_binding_energy
-from .fixed_clu_binding_energy import fixed_clu_binding_energy 
+from .fixed_clu_binding_energy import fixed_clu_binding_energy
+from .zpe_correction           import zpe_correction
--- AutoMD/experiments/zpe_correction.py
+++ AutoMD/experiments/zpe_correction.py
@@ -1,4 +1,32 @@
 from ..config import *
 
-def zpe_correction():
+def zpe_correction(molecule, s0, s1, n, m, saveName):
+
+    def get_EsZPE(xyz):
+        system, _ = prep_system(xyz)
+        E         = system.get_potential_energy()
+        vib       = Vibrations(system, delta=0.0001)
+        vib.run()
+        vib.summary(log=xyz.replace('.xyz', '_vib.out'))
+        zpe       = vib.get_zero_point_energy()
+        vib.clean()
+        EsZPE     = E + zpe
+        return EsZPE
+
+    dic = {}
+    mol = get_EsZPE(molecule)
+
+    for i in range(n):
+        key      = 'Cluster %d' % i
+        dic[key] = []
+        for j in range(m):
+            try:
+                clu = get_EsZPE(s0 % i)
+                ful = get_EsZPE(s1 % (i,j))
+                dic[key].append(ful - clu - mol)
+            except:
+                dic[key].append(np.nan)
+
+    df  = pd.DataFrame(dic)
+    df.to_csv(saveName)
     return
