74a743f813d535f6ddcc14e7259c8ff30cdcd0e9
brian <ferraribrian1996@gmail.com>
2022-09-27 13:25:20 +0200

    update neighbor finder

--- AutoMD/analysis/get_num_neighbors.py
+++ AutoMD/analysis/get_num_neighbors.py
@@ -1,20 +1,28 @@
 from ..config import *
 
-def get_num_neighbors(xyz, mol_index):
+def get_num_neighbors(xyz, mol, D):
     
-    def dist(v1,v2):
-        d = np.linalg.norm(v1 - v2)
+    def dist(v1,v2,v3):
+        a = np.linalg.norm(v1 - v2)
+        b = np.linalg.norm(v1 - v3)
+        d = min([a,b])
         return d
 
-    atoms = read(xyz)
-    coms  = []
-    pos   = atoms.get_positions()
-    mas   = atoms.get_masses()
-    for i in range(len(atoms)//2):
-        a = i*2
-        b = a+2
-        coms.append(CoM(pos[a:b], mas[a:b]))
+    def find_pair(n):
+        if n%2:
+            p = (n-1, n)
+        else:
+            p = (n, n+1)
+        return p
     
-    m     = coms[mol_index]
-    tmp   = [x for x in coms if (dist(x,m) <= 5) and (dist(x,m) > 0.5)]
-    return len(tmp)
+    atoms = read(xyz)
+    tmp   = [x % len(atoms) for x in mol]
+    pos   = atoms.get_positions()
+    a1    = pos[tmp[0]]
+    a2    = pos[tmp[1]]
+    pos   = [list(pos[i]) for i in range(len(pos)) if i not in mol]
+    tmp   = [pos.index(x) for x in pos if dist(x,a1,a2) <= D]
+    tmp   = [find_pair(x) for x in tmp]
+    N     = len(set(tmp))
+    
+    return N
