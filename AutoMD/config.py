78930554825ec9d991f0f7db4b3aad69748128b3
Brian Carmona Ferrari <ferraribrian1996@gmail.com>
2022-11-25 14:49:32 +0100

    now with faster calc

--- AutoMD/config.py
+++ AutoMD/config.py
@@ -7,7 +7,7 @@ from scipy.constants import c
 from ase.io import read, write
 from ase import units, Atoms
 from ase.optimize import BFGS, FIRE, GPMin, MDMin
-from .MvH_CO_JM8_Numba import MvH_CO
+from .MvH_CO_JM8_BCF import MvH_CO
 from ase.visualize import view
 from ase.vibrations import Vibrations
 from ase.md.verlet import VelocityVerlet
