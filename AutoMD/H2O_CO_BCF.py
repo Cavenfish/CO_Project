8d4190c5fa7a3b278226ce00f0092ffb785933e0
brian <ferraribrian1996@gmail.com>
2023-01-16 18:10:45 +0100

    more work on h2oco ff

--- AutoMD/H2O_CO_BCF.py
+++ AutoMD/H2O_CO_BCF.py
@@ -1,7 +1,7 @@
 from os    import path
 from yaml  import load
 from numba import njit
-from numpy import exp, sqrt, dot, zeros_like
+from numpy import exp, sqrt, dot, zeros_like, where
 
 @njit
 def calculate_Morse(r, diff, D, r0, beta):
@@ -9,7 +9,7 @@ def calculate_Morse(r, diff, D, r0, beta):
     expf = exp(beta * (r - r0))
     E    = D * (1 - 2*expf + expf**2)
     F    = preF * expf * (expf - 1) * diff / r
-
+    #TODO: normalize values here 
     return E, F
 
 @njit
@@ -26,6 +26,12 @@ def V_disp(r, diff, Cij):
     return E, F
 
 @njit
+def V_exch(r, diff, Aij, Bij):
+    E = Aij * exp(-Bij * r)
+    F = Bij * E * diff / r
+    return E, F
+
+@njit
 def V_coul(r, diff, Qij):
     E = Qij / r
     F = Qij * diff / r**3
@@ -38,6 +44,65 @@ def V_LJ(r, diff, sigma, epsilon):
     k = 12 * (sigma/r)**11 - 6 * (sigma/r)**5
     F = c * k * diff
     return E, F
+
+@njit
+def calculate_COCO(molA, molB):
+    return
+
+@njit
+def calculate_H2OH2O(molA, molB):
+    return
+
+@njit
+def calculate_COH2O(molA, molB):
+    return
+
+@njit
+def calculate_CO(pos, molA, params, MorseE, MorseF):
+    posi0 = positions[molA[0]]
+    posi1 = positions[molA[1]]
+    ri1i0 = diffDotSqrt(posi1, posi0)
+
+    intraP = params['CO'] if molA in co else params['H2O']
+    Dr0B   = (intraP['D'], intraP['r0'], intraP['beta'])
+
+    #Calculate Morse 
+    E, F             = calculate_Morse(*ri1i0, *Dr0B)
+    MorseE          += E
+    MorseF[molA[0]] -= F
+    MorseF[molA[1]] += F
+
+    return MorseE, MorseF
+
+@njit
+def diffDotSqrt(v2, v1):
+    diff = v2 - v1
+    r    = sqrt(dot(diff,diff))
+    return (r, diff)
+
+@njit
+def executeCalculations(positions, co, water, params):
+    molecs = co.append(water)
+    N      = len(molecs)
+    MorseF = zeros_like(positions)
+    HarmoF = zeros_like(positions)
+    ExchF  = zeros_like(positions)
+    DispF  = zeros_like(positions)
+    CoulF  = zeros_like(positions)
+    MorseE = HarmoE = ExchE = DispE = CoulE = 0.0
+    
+    for i in range(N):
+        molA  = molecules[i]
+        
+        if molA in co:
+            MorseE, MorseF = calculate_CO(positions, molA, params,
+                                          MorseE, MorseF)
+        else:
+            MorseE, = calculate_H2O(positions, molA, params)
+
+
+
+    return
 
 from ase.calculators.calculator import (Calculator, all_changes)
 
@@ -57,6 +122,27 @@ class H2O_CO(Calculator):
 
     def __init__(self, **kwargs):
         Calculator.__init__(self, **kwargs)
+
+    def get_molecules(self, atoms):
+        def get_co(i,d):
+            j = where((d[i] > 0.1) & (d[i] < 1.3))[0][0]
+            return (i,j)
+
+        def get_water(i,d):
+            x = where((d[i] > 0.1) & (d[i] < 1.9))[0] 
+            m = [i,x[0],x[1]]
+            m.sort()
+            return tuple(m)
+
+        symbols   = atoms.get_chemical_symbols()
+        distances = atoms.get_all_distances()
+        N         = range(len(symbols))
+        carbons   = [i for i in N if symbols[i] == 'C']
+        hydrogen  = [i for i in N if symbols[i] == 'H']
+        co        = [get_co(i,distances) for i in carbons]
+        water     = {get_water(i,distances) for i in hydrogen}
+
+        return co, water
 
     def calculate(self, atoms=None, properties=['energy'], 
                   system_changes=all_changes):
