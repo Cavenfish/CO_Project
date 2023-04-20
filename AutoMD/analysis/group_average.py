1150179a2b33b934fa8768d5f9ac6ccd89151209
brian <ferraribrian1996@gmail.com>
2023-04-20 20:02:00 +0200

    added comments to group_average and revamped half_life

--- AutoMD/analysis/group_average.py
+++ AutoMD/analysis/group_average.py
@@ -1,28 +1,35 @@
 from ..config import *
 
 def group_average(h5):
-    hdf = pd.HDFStore(h5) 
-
+    hdf = pd.HDFStore(h5)
     tmp = {}
+
+    # i is [groupName, groupsInGroup, membersInGroup]
     for i in hdf.walk():
         for j in i[-1]:
+
+            # key becomes: groupName - last char + 'Avg'
             key = f'{i[0][:-1]}Avg'
-            
+
+            # Here we just fill dictionary with
+            # strings for getting memebers
             try:
-                tmp[key].append(f'{i[0]}/{j}')
+                tmp[key].append(f'{i[0]}/{j}') # ie. groupName/member
             except:
                 tmp[key] = [f'{i[0]}/{j}']
 
-
+    # Now we run through the dictionary
+    # to get members and average the
+    # DataFrame into a zero-ed out one
     for key in tmp.keys():
         avg = hdf.get(tmp[key][0]) * 0
-        
+
         for i in tmp[key]:
             df   = hdf.get(i)
             avg += df
-        
+
         avg /= len(tmp[key])
-        
+
         hdf.put(key, df)
 
     hdf.close()
--- AutoMD/analysis/half_life.py
+++ AutoMD/analysis/half_life.py
@@ -1,14 +1,39 @@
 from ..config import *
 
-def half_life(directory):
-    tauList = []
-    for filename in os.listdir(directory):
-        df     = pd.read_csv(filename)
-        E, tau = half_life(df, filename.replace('.csv', '.png'))
-        tauList.append(tau)
+#Single exponential decay
+def f(x, E, tau):
+    return (E * np.exp(-x / tau))
 
-    tau_avg = np.average(tauList)
+#Two channel exponential decay
+def g(x, E, tau, E2, tau2):
+    return f(x, E, tau) + f(x, E2, tau2)
 
+#The two channel expoential decay fits the
+#decay curves best. The assumption here is:
+#-> One channel is for vib-vib transfer
+#-> Other is for vib-lattice transfer
+
+def leaves(s):
+    node = hdf.get_node(s + f'{i}')
+    for j in node.__members__:
+        yield f'{s}{i}/{j}'
+
+def half_life(h5):
+    hdf = pd.HDFStore(h5)
+
+    for i in hdf.walk():
+        tmp = {'uid':[], 'E1':[], 'tau1':[], 'E2':[], 'tau2':[]}
+        for j in i[-1]:
+            df = hdf.get(f'{i[0]}/{j}')
+            p0 = (0.3, 100, 0.1, 10)
+
+            popt, pcov = curve_fit(df['Time'], df['Sliced Energy'], p0)
+
+            tmp['uid' ].append(j)
+            tmp['E1'  ].append(popt[0])
+            tmp['tau1'].append(popt[1])
+            tmp['E2'  ].append(popt[2])
+            tmp['tau2'].append(popt[3])
+
+        hdf.put(f'{i[0]}/tau', tmp)
     return
-
-
