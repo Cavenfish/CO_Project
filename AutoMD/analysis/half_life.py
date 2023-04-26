e160d4d23162c298ed7a3994a54d51e6df9e2620
brian <ferraribrian1996@gmail.com>
2023-04-26 18:01:05 +0200

    attempting to make all fits perfect

--- AutoMD/analysis/half_life.py
+++ AutoMD/analysis/half_life.py
@@ -13,27 +13,46 @@ def g(x, E, tau, E2, tau2):
 #-> One channel is for vib-vib transfer
 #-> Other is for vib-lattice transfer
 
-def leaves(s):
-    node = hdf.get_node(s + f'{i}')
-    for j in node.__members__:
-        yield f'{s}{i}/{j}'
+def get_fit(df, p0):
+    dp0 = np.array([0.01, 500, -0.01, 30])
+    x   = df['Time']
+    y   = df['Sliced Energy']
+
+    try:
+        bounds     = ([0,0,0,0],[1,1e10,1,1e4])
+        popt, pcov = curve_fit(g, x, y, p0, bounds=bounds)
+    except RuntimeError:
+        p0 += dp0
+        popt, pcov = get_fit(df, p0)
+
+    return popt, pcov
 
 def half_life(h5):
     hdf = pd.HDFStore(h5)
 
     for i in hdf.walk():
+
+        # Prevent entering in unwanted groups
+        if (len(i[-1]) < 20) or ('VACF' in i[0]):
+            continue
+
         tmp = {'uid':[], 'E1':[], 'tau1':[], 'E2':[], 'tau2':[]}
         for j in i[-1]:
-            df = hdf.get(f'{i[0]}/{j}')
-            p0 = (0.3, 100, 0.1, 10)
+            if not j.isdigit():
+                continue
 
-            popt, pcov = curve_fit(df['Time'], df['Sliced Energy'], p0)
+            df = hdf.get(f'{i[0]}/{j}')
+            p0 = np.array([0.01, 1000, 0.4, 1])
+
+            popt, pcov = get_fit(df, p0)
 
             tmp['uid' ].append(j)
             tmp['E1'  ].append(popt[0])
-            tmp['tau1'].append(popt[1])
+            tmp['tau1'].append(popt[1] * np.log(2))
             tmp['E2'  ].append(popt[2])
-            tmp['tau2'].append(popt[3])
+            tmp['tau2'].append(popt[3] * np.log(2))
 
-        hdf.put(f'{i[0]}/tau', tmp)
+        df = pd.DataFrame(tmp)
+        hdf.put(f'{i[0]}/tau', df)
+    hdf.close()
     return
